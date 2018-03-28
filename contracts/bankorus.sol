97fe35036f7e55dcf44afc917125f16379438ff1
AT <tikonoff@gmail.com>
2018-03-29 11:18:07 +1300

    new contract and scripts

--- contracts/bankorus.sol
+++ contracts/bankorus.sol
@@ -4,75 +4,47 @@ import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
 import 'zeppelin-solidity/contracts/ownership/rbac/RBAC.sol';
 
+contract bankorus is MintableToken, Pausable, RBAC {
+  string public name = "Bankorus";
+  string public symbol = "BKT";
+  string public roleFrozen = "FROZEN";
+  uint8 public decimals = 18;
 
-/**
- * @title BankorusToken
- * @dev ERC20 Bankorus Token (BKT)
-  *
- * All initial BKT tokens are assigned to the creator of
- * this contract.
- *
- */
+  function bankorus(uint256 _totalSupply) public {
+    totalSupply_ = _totalSupply * 10 ** uint256(decimals);
+    balances[owner] = totalSupply_;
+  }
 
+  function transfer(address _to, uint256 _value) public returns (bool success) {
 
-contract BankorusToken is MintableToken, Pausable, RBAC {
-    string public name = "Bankorus";    // Set the token name for display
-    string public symbol = "BKT";        // Set the token symbol for display
-    string public roleFrozen = "FROZEN";  // Set the initial RBAC state to “FROZEN”
-    uint8 public decimals = 18;          // Set the number of decimals for display
+    require(!hasRole(_to, roleFrozen));
+    require(!hasRole(msg.sender, roleFrozen));
 
-    // initialize balances
-    mapping (address => uint256) balances;
-
-
-    /**
-     * @dev BankorusToken Constructor
-     * Runs only on initial contract creation.
-     */
-
-    function BankorusToken (uint256 initialSupply) public {
-        totalSupply_ = totalSupply * 10 ** uint256(decimals);
-        balances[owner] = totalSupply_;  // Creator address is assigned all initial tokens
-        contractAddress = this;
-        Transfer(0x0, owner, totalSupply_); // Send initial supply of tokens to creator
+    if (!paused) {
+      require(msg.sender == owner);
     }
+    return super.transfer(_to, _value);
+  }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(!hasRole(_to, roleFrozen));
+    require(!hasRole(msg.sender, roleFrozen));
 
-        require(!hasRole(_to, roleFrozen));
-        require(!hasRole(msg.sender, roleFrozen));
+    return super.transferFrom(_from, _to, _value);
+  }
 
-        if (!paused) {
-            require(msg.sender == owner);
-        }
-        return super.transfer(_to, _value);
-    }
+  function frozen(address target) public {    
+    checkRole(msg.sender, ROLE_ADMIN);
+    addRole(target, roleFrozen);
+  }
 
+  function unFrozen(address target) public {
+    checkRole(msg.sender, ROLE_ADMIN);
+    removeRole(target, roleFrozen);
+  }
 
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(!hasRole(_to, roleFrozen));
-        require(!hasRole(msg.sender, roleFrozen));
-        require(paused);
-
-        return super.transferFrom(_from, _to, _value);
-    }
-
-
-
-    function frozen(address target) public {
-        checkRole(msg.sender, ROLE_ADMIN);
-        addRole(target, roleFrozen);
-    }
-
-    function unFrozen(address target) public {
-        checkRole(msg.sender, ROLE_ADMIN);
-        removeRole(target, roleFrozen);
-    }
-
-    function allowance(address _spender) public view returns (uint256) {
-        return super.allowance(msg.sender, _spender);
-    }
+  function allowance(address _spender) public view returns (uint256) {
+    return super.allowance(msg.sender, _spender);
+  }
 
 }
-
--- contracts/flattened_bankorus.sol
+++ contracts/flattened_bankorus.sol
@@ -328,7 +328,7 @@ library SafeMath {
   }
 
   /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
   */
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
@@ -563,73 +563,47 @@ contract MintableToken is StandardToken, Ownable {
 
 // File: contracts/bankorus.sol
 
-/**
- * @title BankorusToken
- * @dev ERC20 Bankorus Token (BKT)
-  *
- * All initial BKT tokens are assigned to the creator of
- * this contract.
- *
- */
+contract bankorus is MintableToken, Pausable, RBAC {
+  string public name = "Bankorus";
+  string public symbol = "BKT";
+  string public roleFrozen = "FROZEN";
+  uint8 public decimals = 18;
 
+  function bankorus(uint256 _totalSupply) public {
+    totalSupply_ = _totalSupply * 10 ** uint256(decimals);
+    balances[owner] = totalSupply_;
+  }
 
-contract BankorusToken is MintableToken, Pausable, RBAC {
-    string public name = "Bankorus";    // Set the token name for display
-    string public symbol = "BKT";        // Set the token symbol for display
-    string public roleFrozen = "FROZEN";  // Set the initial RBAC state to “FROZEN”
-    uint8 public decimals = 18;          // Set the number of decimals for display
+  function transfer(address _to, uint256 _value) public returns (bool success) {
 
-    // initialize balances
-    mapping (address => uint256) balances;
+    require(!hasRole(_to, roleFrozen));
+    require(!hasRole(msg.sender, roleFrozen));
 
-
-    /**
-     * @dev BankorusToken Constructor
-     * Runs only on initial contract creation.
-     */
-
-    function BankorusToken (uint256 initialSupply) public {
-        totalSupply_ = initialSupply * 10 ** uint256(decimals);
-        balances[owner] = totalSupply_;  // Creator address is assigned all initial tokens
-        contractAddress = this;
-        Transfer(0x0, owner, totalSupply_); // Send initial supply of tokens to creator
+    if (!paused) {
+      require(msg.sender == owner);
     }
+    return super.transfer(_to, _value);
+  }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(!hasRole(_to, roleFrozen));
+    require(!hasRole(msg.sender, roleFrozen));
 
-        require(!hasRole(_to, roleFrozen));
-        require(!hasRole(msg.sender, roleFrozen));
+    return super.transferFrom(_from, _to, _value);
+  }
 
-        if (!paused) {
-            require(msg.sender == owner);
-        }
-        return super.transfer(_to, _value);
-    }
+  function frozen(address target) public {    
+    checkRole(msg.sender, ROLE_ADMIN);
+    addRole(target, roleFrozen);
+  }
 
+  function unFrozen(address target) public {
+    checkRole(msg.sender, ROLE_ADMIN);
+    removeRole(target, roleFrozen);
+  }
 
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(!hasRole(_to, roleFrozen));
-        require(!hasRole(msg.sender, roleFrozen));
-        require(paused);
-
-        return super.transferFrom(_from, _to, _value);
-    }
-
-
-
-    function frozen(address target) public {
-        checkRole(msg.sender, ROLE_ADMIN);
-        addRole(target, roleFrozen);
-    }
-
-    function unFrozen(address target) public {
-        checkRole(msg.sender, ROLE_ADMIN);
-        removeRole(target, roleFrozen);
-    }
-
-    function allowance(address _spender) public view returns (uint256) {
-        return super.allowance(msg.sender, _spender);
-    }
+  function allowance(address _spender) public view returns (uint256) {
+    return super.allowance(msg.sender, _spender);
+  }
 
 }
