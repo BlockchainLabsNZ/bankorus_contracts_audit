7b78a851eed89f19c237442baa68c01d84c17272
Felix Watkins <felixwatkinz@gmail.com>
2018-04-03 09:26:57 +1200

    remove outdated files

--- migrations/2_initial_bankorus.js
+++
@@ -1,6 +0,0 @@
-var bk = artifacts.require("./bankorus.sol")
-
-module.exports = function(deployer) {
-  // Use deployer to state migration tasks.
-  deployer.deploy(bk, 1000);
-};
--- scripts/README.md
+++
@@ -1,4 +0,0 @@
-# how to use
-1. password for wallet
-2. wallet file
-3. python companycoin.py
